2bb9e10cf64dcabef2255e16e3c7cb5dedf6f535
rexykilller441-maker <rexykilller441@gmail.com>
2025-11-22 21:36:06 +0530

    Update TotemCommand.java

--- src/main/java/com/autototem/TotemCommand.java
+++ src/main/java/com/autototem/TotemCommand.java
@@ -1,67 +1,100 @@
 package com.autototem;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
-
-import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
+import net.minecraft.class_2561;
 
 public class TotemCommand {
-
-    public static void register(CommandDispatcher<FabricClientCommandSource> dispatcher) {
-
-        dispatcher.register(literal("tt")
-                // Toggle entire mod
-                .then(literal("toggle")
-                        .executes(context -> {
-                            AutoInventoryTotem.toggle();
-                            context.getSource().sendFeedback("AutoTotem: " +
-                                    (AutoInventoryTotem.isEnabled() ? "§aEnabled" : "§cDisabled"));
-                            return 1;
-                        })
-                )
-
-                // Toggle Offhand Mode
-                .then(literal("offhand")
-                        .executes(context -> {
-                            AutoInventoryTotem.ENABLE_OFFHAND = !AutoInventoryTotem.ENABLE_OFFHAND;
-                            context.getSource().sendFeedback("Offhand Refill: " +
-                                    (AutoInventoryTotem.ENABLE_OFFHAND ? "§aEnabled" : "§cDisabled"));
-                            return 1;
-                        })
-                )
-
-                // Toggle Hotbar Mode
-                .then(literal("hotbar")
-                        .executes(context -> {
-                            AutoInventoryTotem.ENABLE_HOTBAR = !AutoInventoryTotem.ENABLE_HOTBAR;
-                            context.getSource().sendFeedback("Hotbar Refill: " +
-                                    (AutoInventoryTotem.ENABLE_HOTBAR ? "§aEnabled" : "§cDisabled"));
-                            return 1;
-                        })
-                )
-
-                // Toggle Double Totem Mode
-                .then(literal("double")
-                        .executes(context -> {
-                            AutoInventoryTotem.ENABLE_DOUBLE_TOTEM = !AutoInventoryTotem.ENABLE_DOUBLE_TOTEM;
-                            context.getSource().sendFeedback("Double-Totem Mode: " +
-                                    (AutoInventoryTotem.ENABLE_DOUBLE_TOTEM ? "§aEnabled" : "§cDisabled"));
-                            return 1;
-                        })
-                )
-
-                // Set hotbar slot
-                .then(literal("slot")
-                        .then(net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument(
-                                        "id", com.mojang.brigadier.arguments.IntegerArgumentType.integer(0, 8))
-                                .executes(context -> {
-                                    int id = com.mojang.brigadier.arguments.IntegerArgumentType.getInteger(context, "id");
-                                    AutoInventoryTotem.setConfiguredSlot(id);
-                                    context.getSource().sendFeedback("Totem Hotbar Slot Set To: §e" + id);
-                                    return 1;
-                                })
-                        )
-                )
+    
+    public static void register() {
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+            registerCommand(dispatcher);
+        });
+    }
+    
+    private static void registerCommand(CommandDispatcher<FabricClientCommandSource> dispatcher) {
+        dispatcher.register(ClientCommandManager.literal("tt")
+            .then(ClientCommandManager.literal("slot")
+                .then(ClientCommandManager.argument("slotNumber", IntegerArgumentType.integer(1, 9))
+                    .executes(TotemCommand::setSlot)))
+            .then(ClientCommandManager.literal("delay")
+                .then(ClientCommandManager.literal("doublehand")
+                    .then(ClientCommandManager.argument("ticks", IntegerArgumentType.integer(0, 100))
+                        .executes(context -> setDelay(context, "doublehand"))))
+                .then(ClientCommandManager.literal("openinventory")
+                    .then(ClientCommandManager.argument("ticks", IntegerArgumentType.integer(0, 100))
+                        .executes(context -> setDelay(context, "openinventory"))))
+                .then(ClientCommandManager.literal("switchtotems")
+                    .then(ClientCommandManager.argument("ticks", IntegerArgumentType.integer(0, 100))
+                        .executes(context -> setDelay(context, "switchtotems"))))
+                .then(ClientCommandManager.literal("closeinventory")
+                    .then(ClientCommandManager.argument("ticks", IntegerArgumentType.integer(0, 100))
+                        .executes(context -> setDelay(context, "closeinventory")))))
+            .then(ClientCommandManager.literal("info")
+                .executes(TotemCommand::showInfo))
         );
     }
-                                        }
+    
+    private static int setSlot(CommandContext<FabricClientCommandSource> context) {
+        int slotNumber = IntegerArgumentType.getInteger(context, "slotNumber");
+        int slotIndex = slotNumber - 1; // Convert to 0-indexed
+        
+        AutoInventoryTotem.setConfiguredSlot(slotIndex);
+        context.getSource().sendFeedback(
+            class_2561.method_43470("§6[AutoTotem] §aConfigured hotbar slot set to: §e" + slotNumber)
+        );
+        
+        return 1;
+    }
+    
+    private static int setDelay(CommandContext<FabricClientCommandSource> context, String delayType) {
+        int ticks = IntegerArgumentType.getInteger(context, "ticks");
+        
+        switch (delayType) {
+            case "doublehand":
+                AutoInventoryTotem.setDoubleHandDelay(ticks);
+                context.getSource().sendFeedback(
+                    class_2561.method_43470("§6[AutoTotem] §aDouble hand delay set to: §e" + ticks + " ticks")
+                );
+                break;
+            case "openinventory":
+                AutoInventoryTotem.setOpenInventoryDelay(ticks);
+                context.getSource().sendFeedback(
+                    class_2561.method_43470("§6[AutoTotem] §aOpen inventory delay set to: §e" + ticks + " ticks")
+                );
+                break;
+            case "switchtotems":
+                AutoInventoryTotem.setSwitchTotemsDelay(ticks);
+                context.getSource().sendFeedback(
+                    class_2561.method_43470("§6[AutoTotem] §aSwitch totems delay set to: §e" + ticks + " ticks")
+                );
+                break;
+            case "closeinventory":
+                AutoInventoryTotem.setCloseInventoryDelay(ticks);
+                context.getSource().sendFeedback(
+                    class_2561.method_43470("§6[AutoTotem] §aClose inventory delay set to: §e" + ticks + " ticks")
+                );
+                break;
+        }
+        
+        return 1;
+    }
+    
+    private static int showInfo(CommandContext<FabricClientCommandSource> context) {
+        int slot = AutoInventoryTotem.getConfiguredSlot() + 1;
+        context.getSource().sendFeedback(class_2561.method_43470("§6§m-------------------§r §6[AutoTotem Info]§r §6§m-------------------"));
+        context.getSource().sendFeedback(class_2561.method_43470("§eStatus: " + (AutoInventoryTotem.isModEnabled() ? "§aEnabled" : "§cDisabled")));
+        context.getSource().sendFeedback(class_2561.method_43470("§eConfigured Slot: §b" + slot));
+        context.getSource().sendFeedback(class_2561.method_43470("§eDouble Hand Delay: §b" + AutoInventoryTotem.getDoubleHandDelay() + " ticks"));
+        context.getSource().sendFeedback(class_2561.method_43470("§eOpen Inventory Delay: §b" + AutoInventoryTotem.getOpenInventoryDelay() + " ticks"));
+        context.getSource().sendFeedback(class_2561.method_43470("§eSwitch Totems Delay: §b" + AutoInventoryTotem.getSwitchTotemsDelay() + " ticks"));
+        context.getSource().sendFeedback(class_2561.method_43470("§eClose Inventory Delay: §b" + AutoInventoryTotem.getCloseInventoryDelay() + " ticks"));
+        context.getSource().sendFeedback(class_2561.method_43470("§6§m-----------------------------------------------------"));
+        
+        return 1;
+    }
+}
