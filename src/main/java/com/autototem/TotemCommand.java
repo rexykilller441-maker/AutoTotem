2d0a6263d11f93a017bc0a4247776ea887a5f0a3
rexykilller441-maker <rexykilller441@gmail.com>
2025-11-22 21:45:27 +0530

    Update TotemCommand.java

--- src/main/java/com/autototem/TotemCommand.java
+++ src/main/java/com/autototem/TotemCommand.java
@@ -6,7 +6,7 @@ import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
-import net.minecraft.class_2561;
+import net.minecraft.text.Text;
 
 public class TotemCommand {
     
@@ -45,7 +45,7 @@ public class TotemCommand {
         
         AutoInventoryTotem.setConfiguredSlot(slotIndex);
         context.getSource().sendFeedback(
-            class_2561.method_43470("§6[AutoTotem] §aConfigured hotbar slot set to: §e" + slotNumber)
+            Text.literal("§6[AutoTotem] §aConfigured hotbar slot set to: §e" + slotNumber)
         );
         
         return 1;
@@ -58,25 +58,25 @@ public class TotemCommand {
             case "doublehand":
                 AutoInventoryTotem.setDoubleHandDelay(ticks);
                 context.getSource().sendFeedback(
-                    class_2561.method_43470("§6[AutoTotem] §aDouble hand delay set to: §e" + ticks + " ticks")
+                    Text.literal("§6[AutoTotem] §aDouble hand delay set to: §e" + ticks + " ticks")
                 );
                 break;
             case "openinventory":
                 AutoInventoryTotem.setOpenInventoryDelay(ticks);
                 context.getSource().sendFeedback(
-                    class_2561.method_43470("§6[AutoTotem] §aOpen inventory delay set to: §e" + ticks + " ticks")
+                    Text.literal("§6[AutoTotem] §aOpen inventory delay set to: §e" + ticks + " ticks")
                 );
                 break;
             case "switchtotems":
                 AutoInventoryTotem.setSwitchTotemsDelay(ticks);
                 context.getSource().sendFeedback(
-                    class_2561.method_43470("§6[AutoTotem] §aSwitch totems delay set to: §e" + ticks + " ticks")
+                    Text.literal("§6[AutoTotem] §aSwitch totems delay set to: §e" + ticks + " ticks")
                 );
                 break;
             case "closeinventory":
                 AutoInventoryTotem.setCloseInventoryDelay(ticks);
                 context.getSource().sendFeedback(
-                    class_2561.method_43470("§6[AutoTotem] §aClose inventory delay set to: §e" + ticks + " ticks")
+                    Text.literal("§6[AutoTotem] §aClose inventory delay set to: §e" + ticks + " ticks")
                 );
                 break;
         }
@@ -86,15 +86,15 @@ public class TotemCommand {
     
     private static int showInfo(CommandContext<FabricClientCommandSource> context) {
         int slot = AutoInventoryTotem.getConfiguredSlot() + 1;
-        context.getSource().sendFeedback(class_2561.method_43470("§6§m-------------------§r §6[AutoTotem Info]§r §6§m-------------------"));
-        context.getSource().sendFeedback(class_2561.method_43470("§eStatus: " + (AutoInventoryTotem.isModEnabled() ? "§aEnabled" : "§cDisabled")));
-        context.getSource().sendFeedback(class_2561.method_43470("§eConfigured Slot: §b" + slot));
-        context.getSource().sendFeedback(class_2561.method_43470("§eDouble Hand Delay: §b" + AutoInventoryTotem.getDoubleHandDelay() + " ticks"));
-        context.getSource().sendFeedback(class_2561.method_43470("§eOpen Inventory Delay: §b" + AutoInventoryTotem.getOpenInventoryDelay() + " ticks"));
-        context.getSource().sendFeedback(class_2561.method_43470("§eSwitch Totems Delay: §b" + AutoInventoryTotem.getSwitchTotemsDelay() + " ticks"));
-        context.getSource().sendFeedback(class_2561.method_43470("§eClose Inventory Delay: §b" + AutoInventoryTotem.getCloseInventoryDelay() + " ticks"));
-        context.getSource().sendFeedback(class_2561.method_43470("§6§m-----------------------------------------------------"));
+        context.getSource().sendFeedback(Text.literal("§6§m-------------------§r §6[AutoTotem Info]§r §6§m-------------------"));
+        context.getSource().sendFeedback(Text.literal("§eStatus: " + (AutoInventoryTotem.isModEnabled() ? "§aEnabled" : "§cDisabled")));
+        context.getSource().sendFeedback(Text.literal("§eConfigured Slot: §b" + slot));
+        context.getSource().sendFeedback(Text.literal("§eDouble Hand Delay: §b" + AutoInventoryTotem.getDoubleHandDelay() + " ticks"));
+        context.getSource().sendFeedback(Text.literal("§eOpen Inventory Delay: §b" + AutoInventoryTotem.getOpenInventoryDelay() + " ticks"));
+        context.getSource().sendFeedback(Text.literal("§eSwitch Totems Delay: §b" + AutoInventoryTotem.getSwitchTotemsDelay() + " ticks"));
+        context.getSource().sendFeedback(Text.literal("§eClose Inventory Delay: §b" + AutoInventoryTotem.getCloseInventoryDelay() + " ticks"));
+        context.getSource().sendFeedback(Text.literal("§6§m-----------------------------------------------------"));
         
         return 1;
     }
-}
+            }
